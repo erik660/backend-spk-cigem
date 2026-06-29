af9f4e9a4121b0679d1fa847389b8b34bbc99502
Muhammad Erico Revaldo <email_github_erikorevaldo4@gmail.com>
2026-06-29 12:30:07 +0700

    fix db config

--- backend_cigem/settings.py
+++ backend_cigem/settings.py
@@ -53,11 +53,11 @@ WSGI_APPLICATION = 'backend_cigem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE') or os.environ.get('DB_NAME') or 'cigem_db',
-        'USER': os.environ.get('MYSQLUSER') or os.environ.get('DB_USER') or 'root',
-        'PASSWORD': os.environ.get('MYSQLPASSWORD') or os.environ.get('DB_PASSWORD') or '',
-        'HOST': os.environ.get('MYSQLHOST') or os.environ.get('DB_HOST') or 'localhost',
-        'PORT': os.environ.get('MYSQLPORT') or os.environ.get('DB_PORT') or '3306',
+        'NAME': os.environ.get('MYSQL_DATABASE', 'railway'),
+        'USER': os.environ.get('MYSQLUSER', 'root'),
+        'PASSWORD': os.environ.get('MYSQLPASSWORD', ''),
+        'HOST': os.environ.get('MYSQLHOST', '127.0.0.1'),
+        'PORT': os.environ.get('MYSQLPORT', '3306'),
     }
 }
 
